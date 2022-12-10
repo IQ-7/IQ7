660f1ab68bdb19cb8a289a66c9c99aeea17495a9
IQ7 <iq7.dev@gmail.com>
2022-12-10 16:15:10 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Project-Xtended/manifest.git -b xq -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/IQ-7/manifest.git -b xq -g default,-mips,-darwin,-notdefault
 git clone https://github.com/IQ-7/local_manifest --depth 1 -b xq-whyred .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
@@ -15,7 +15,7 @@ mkfifo reading # Jangan di Hapus
 tee "${BUILDLOG}" < reading & # Jangan di Hapus
 build_message "Building Started" # Jangan di Hapus
 progress & # Jangan di Hapus
-make xtended -j8  > reading #& sleep 95m # Jangan di hapus text line (> reading)
+make xtended -j8  > reading & sleep 95m # Jangan di hapus text line (> reading)
 
 retVal=$?
 timeEnd
